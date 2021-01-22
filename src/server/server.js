ccb05dd87dc0953baf5313a3e03d553387409ab8
“Alondra <alondra.casur@gmail.com>
2021-01-22 14:47:50 -0500

    ok 5

--- src/server/server.js
+++ src/server/server.js
@@ -42,7 +42,7 @@ if (ENV === 'development') {
   app.use(webpackHotMiddleware(compiler));
 } else {
   app.use((req, res, next) => {
-    req.hashManifest = getManifest(); //check this out
+    if (!req.hashManifest) res.hashManifest = getManifest(); //check this out
     next();
   });
   app.use(helmet());
